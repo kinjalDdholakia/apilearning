cd4680698cddac960f2f06d62608de2f20f39e7c
kinjalDdholakia <kinjal.arsenal@gmail.com>
2024-02-22 13:09:31 +0530

    implement repository pattern

---
+++ app/Interfaces/StudentRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Interfaces;
+interface StudentRepositoryInterface
+{
+    public function GetTopStudents_ClassWise();
+    public function GetTopStudents_SubjectWise();
+    public function GetTopStudents_Last_5_Years();
+
+}
